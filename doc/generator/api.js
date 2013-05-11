ae22439a8674bdda90972ca938d20900239b0a3e
Olivier Sallou <olivier.sallou@irisa.fr>
2013-05-11 10:00:31 +0200

    yuidoc generated doc update

---
+++ doc/generator/api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BandIt",
+        "BanditLogger",
+        "pick"
+    ],
+    "modules": [
+        "BandIt"
+    ],
+    "allModules": [
+        {
+            "displayName": "BandIt",
+            "name": "BandIt",
+            "description": "The BandIt module provides methods to draw a workflow<br/>\nAuthor: Olivier Sallou <olivier.sallou@irisa.fr></br>\nLicense: CeCILL-B"
+        }
+    ]
+} };
+});
